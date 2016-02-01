b460d8fef5eda07924781ab387c3433e75670d52
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-01 23:02:07 +0000

    Modified sbr.py

--- sbr.py
+++ sbr.py
@@ -1,5 +1,19 @@
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-import os
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class SBRHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200,"OK")
+        self.send_header('content-type','text/plain')
+        self.end_headers()
+        self.wfile.write("""<HTML>
+        <HEAD><TITLE></TITLE></HEAD>
+        <BODY>Hello</BODY>
+        </HTML>
+        """)
+        pass
+    def do_POST(self):
+        pass
+    def do_PUT(self):
+        pass
 serv = HTTPServer(("",8080),SimpleHTTPRequestHandler)
 serv.serve_forever()
