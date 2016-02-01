111a3fdc03bc1576f73ad32f36f04a5cf6937fa4
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-01 23:03:49 +0000

    Modified sbr.py

--- sbr.py
+++ sbr.py
@@ -3,7 +3,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 class SBRHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200,"OK")
-        self.send_header('content-type','text/plain')
+        self.send_header('content-type','text/html')
         self.end_headers()
         self.wfile.write("""<HTML>
         <HEAD><TITLE></TITLE></HEAD>
