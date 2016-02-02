9ef0ec0b047c41338b887852ff0f407833d3e20d
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-02 00:00:39 +0000

    Modified sbr.py

--- sbr.py
+++ sbr.py
@@ -4,7 +4,7 @@ import json
 class SBRHandler(BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         self.data = {}
-        super(ChildClass, self).__init__(*args, **kwargs)
+        super(BaseHTTPRequestHandler, self).__init__(*args, **kwargs)
         
     def do_GET(self):
         self.send_response(200,"OK")
