dfdb7e08a640c5c306951406903add22f916300c
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-02 02:10:33 +0000

    Modified sbr.py

--- sbr.py
+++ sbr.py
@@ -2,6 +2,13 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import json
 import RPi.GPIO as GPIO
 
+class MotorMgr(object):
+    def __init__(fwdPin,revPin,speed):
+        self = {}
+        self['fwdPin'] = fwdPin
+        self['revPin'] = revPin
+        self['speed'] = speed
+        
 class SBRHandler(BaseHTTPRequestHandler, object):
     def setup(self):
         GPIO.setmode(GPIO.BOARD)
@@ -9,7 +16,8 @@ class SBRHandler(BaseHTTPRequestHandler, object):
     def __init__(self, *args, **kwargs):
         self.data = { 'status': 'ok' }
         super(SBRHandler, self).__init__(*args,**kwargs)
-        
+
+
     def do_GET(self):
         self.send_response(200,"OK")
         self.send_header('content-type','text/json')
