96a47c65279f69951430309079319a8f6c63ef9b
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-02 06:55:05 +0000

    Modified sbr.py

--- sbr.py
+++ sbr.py
@@ -3,12 +3,13 @@ import json
 import RPi.GPIO as GPIO
 
 class MotorIf(object):
-    
+    """    
     def __getitem__(self, key):
         return object.__getattr__(self, key)
 
     def __setitem__(self, key, value):
         object.__setattr__(self, key, value)
+    """
     
     def __init__(self, fwdPin, revPin, pwmPin):
         GPIO.setup(fwdPin,GPIO.OUT)
