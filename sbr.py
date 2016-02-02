de2584335fa72a122daf3f91db6d01257df99d9e
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-02 06:52:24 +0000

    Modified sbr.py

--- sbr.py
+++ sbr.py
@@ -61,7 +61,7 @@ class SBRHandler(BaseHTTPRequestHandler, object):
         self['mR'].set_direction('FORWARD')
 
     def __getitem__(self, key):
-        return getattr(self, key)
+        return object.__getattr__(self, key)
     
     def __setitem__(self, key, value):
         object.__setattr__(self, key, value)
