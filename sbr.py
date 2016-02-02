b8be201d46226754ec981df482d3597770855a8a
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-02 07:06:12 +0000

    Modified sbr.py

--- sbr.py
+++ sbr.py
@@ -58,14 +58,6 @@ class SBRHandler(BaseHTTPRequestHandler):
         self.mL.set_direction('FORWARD')
         self.mR.set_direction('FORWARD')
 
-    """
-    def __getitem__(self, key):
-        return object.__getattr__(self, key)
-    
-    def __setitem__(self, key, value):
-        object.__setattr__(self, key, value)
-    """
-    
     def __init__(self, *args, **kwargs):
         self.data = { 'status': 'ok' }
         self.setup()
