9c268560f809df1948066851ec3e1e9c773e4c03
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-02 13:48:19 +0000

    Modified accelerometer.py

--- accelerometer.py
+++ accelerometer.py
@@ -5,7 +5,7 @@ import math
 
 class Accelerometer(object):
 	
-	def __init__(self,i2caddr=0x68):
+	def __init__(self,i2caddr):
         self.power_mgmt_1 = 0x6b
         self.power_mgmt_2 = 0x6c
         self.bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
