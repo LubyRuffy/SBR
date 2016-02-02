0677152de9916326c37250f848705e8ab1355961
Vivek Srivastav <vsrivastav@gmail.com>
2016-02-02 06:58:22 +0000

    Modified sbr.py

--- sbr.py
+++ sbr.py
@@ -51,13 +51,14 @@ class MotorIf(object):
 class SBRHandler(BaseHTTPRequestHandler, object):
     def setup(self):
         GPIO.setmode(GPIO.BOARD)
-        self['mL'] = MotorIf(33,35,37)
-        self['mR'] = MotorIf(36,38,40)
+        self.mL = MotorIf(33,35,37)
+        self.mR = MotorIf(36,38,40)
+        """
         self['mR'].set_speed(50)
         self['mL'].set_speed(50)
         self['mL'].set_direction('FORWARD')
         self['mR'].set_direction('FORWARD')
-
+        """
     """
     def __getitem__(self, key):
         return object.__getattr__(self, key)
